c8eb352deecd27776c7704229f845504c6431f0b
mulyoved <mulyoved@gmail.com>
2012-09-09 13:33:41 +0300

    Update coursera_downloader.js

--- coursera_downloader.js
+++ coursera_downloader.js
@@ -38,7 +38,7 @@ function buildCommand(){
     var sectionName = $(this).text().replace(/Chapter .+ - /,"").replace(/\:/,'-').replace(/^(V|I|X)+\. /,'');
     $(this).parent().next().find("a.lecture-link").each(function(videoIndex){
       var $lectureLink = $(this);
-      var videoName = $.trim($lectureLink.text()).replace("/","_").replace("/","_").replace("/","_").replace("/","_").replace("/","_");
+      var videoName = $.trim($lectureLink.text()).replace("/","_").replace("/","_").replace("/","_").replace("/","_").replace(":","_");
       //videoName = videoName.replace(":","_").videoName.replace(":","_");
       var downloadLink = $lectureLink.attr('href').replace('/lecture/','/lecture/download.mp4?lecture_id=');
       var cookieHeader = ' --header \"Cookie:'+ document.cookie + '\" ';
